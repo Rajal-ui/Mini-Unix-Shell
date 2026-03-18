a720b90afef195c1fb69d4a842d96ba5f386875c
Rajal <rajalmistry544@gmail.com>
2026-03-18 23:29:26 +0530

    Updated main.c and Makefile

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>   
 #include <stdlib.h>  
 #include <string.h> 
+#include "parser.h"
 
 #define MAX_INPUT 1024  
 
 int main(void) {
     char input[MAX_INPUT];  
+    char *args[MAX_ARGS];
 
     while (1) {  
 
@@ -17,8 +19,12 @@ int main(void) {
          }
 
         input[strcspn(input, "\n")] = '\0'; 
-        if (input[0] == '\0') continue;     
-        printf("You typed: [%s]\n", input);
+        if (input[0] == '\0') continue;  
+        
+        int count = tokenize(input, args);
+        for(int i = 0; i < count; i++) {
+            printf("args[%d] = \"%s\"\n", i, args[i]);
+        }
     }
 
     return 0;
