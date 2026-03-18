cafd85b46b7a221089d8eb5e74a8c11d4e7d4884
Rajal <rajalmistry544@gmail.com>
2026-03-19 00:33:09 +0530

    executor.c updated

--- executor.c
+++ executor.c
@@ -1,9 +1,9 @@
 #include "executor.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>    
-#include <sys/types.h> 
-#include <sys/wait.h>  
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int execute(char **args) {
     pid_t pid;
@@ -12,15 +12,16 @@ int execute(char **args) {
     pid = fork();
 
     if (pid == 0) {
-        execvp(args[0],args);
+        execvp(args[0], args);
+        // only reached if execvp failed
         fprintf(stderr, "mysh: %s: command not found\n", args[0]);
-        exit(127);  
-    } 
+        exit(127);
+    }
     else if (pid > 0) {
-        waitpid(pid, &status, 0);      
+        waitpid(pid, &status, 0);
         return WEXITSTATUS(status);
-
-    } else {
+    }
+    else {
         perror("mysh: fork");
         return -1;
     }
