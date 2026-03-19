541313939dfb2877b5587c7e1463042071047e7d
Rajal <rajalmistry544@gmail.com>
2026-03-19 23:40:15 +0530

    Updated parser.c to detect and strip redirections

--- parser.c
+++ parser.c
@@ -2,17 +2,34 @@
 #include <string.h>  
 #include <stdio.h>
 
-int tokenize(char *input, char **args) {
+int tokenize(char *input, char **args, Redirect *redir) {
+    
+    redir->infile  = NULL;
+    redir->outfile = NULL;
+    redir->append  = 0;
+
     int count = 0;
     char *token = strtok(input, " \t");
 
     while (token != NULL && count < MAX_ARGS - 1) {
-        args[count] = token; 
-        count++;
+
+        if (strcmp(token, ">>") == 0) {            
+            redir->outfile = strtok(NULL, " \t");
+            redir->append  = 1;
+
+        } else if (strcmp(token, ">") == 0) {           
+            redir->outfile = strtok(NULL, " \t");
+            redir->append  = 0;
+
+        } else if (strcmp(token, "<") == 0) {            
+            redir->infile = strtok(NULL, " \t");
+
+        } else {           
+            args[count++] = token;
+        }
 
         token = strtok(NULL, " \t");
     }
-
-    args[count] = NULL;  
+    args[count] = NULL;
     return count;
 }
