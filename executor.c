17e4d218903e9a841bfe30ca42d01f33fd46c6aa
Rajal <rajalmistry544@gmail.com>
2026-03-19 23:43:08 +0530

    applied\ redirection in executor.c

--- executor.c
+++ executor.c
@@ -4,24 +4,46 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include "parser.h"
+#include <fcntl.h> 
 
-int execute(char **args) {
-    pid_t pid;
-    int status;
-
-    pid = fork();
+int execute(char **args, Redirect *redir) {
+    pid_t pid = fork();
 
     if (pid == 0) {
+        if (redir->outfile != NULL) {
+            int flags = O_WRONLY | O_CREAT;
+            flags |= redir->append ? O_APPEND : O_TRUNC;
+
+            int fd = open(redir->outfile, flags, 0644);
+            if (fd < 0) {
+                perror("mysh: open");
+                exit(1);
+            }
+         
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+        }
+        
+        if (redir->infile != NULL) {
+            int fd = open(redir->infile, O_RDONLY);
+            if (fd < 0) {
+                perror("mysh: open");
+                exit(1);
+            }
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+        }
+
         execvp(args[0], args);
-        // only reached if execvp failed
         fprintf(stderr, "mysh: %s: command not found\n", args[0]);
         exit(127);
-    }
-    else if (pid > 0) {
+
+    } else if (pid > 0) {
+        int status;
         waitpid(pid, &status, 0);
         return WEXITSTATUS(status);
-    }
-    else {
+    } else {
         perror("mysh: fork");
         return -1;
     }
