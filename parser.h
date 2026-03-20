b760d999cce3478e3475d9e7a961e55a56010738
Rajal <rajalmistry544@gmail.com>
2026-03-20 16:31:33 +0530

    Updated parser to detect pipes

--- parser.h
+++ parser.h
@@ -2,6 +2,7 @@
 #define PARSER_H
 
 #define MAX_ARGS 64 
+#define MAX_PIPES 16
 
 typedef struct {
     char *infile;   
@@ -9,7 +10,12 @@ typedef struct {
     int  append;  
 } Redirect;
 
+typedef struct {
+    char  *args[MAX_ARGS]; 
+    Redirect redir;        
+} Command;
 
+int parse_pipeline(char *input, Command cmds[]);
 int tokenize(char *input, char **args, Redirect *redir);
 
 #endif
